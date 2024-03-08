5b224f842cc7d81f7d1fd2060078973f48b30c06
adko <154256737+adkooo@users.noreply.github.com>
2024-03-08 04:30:26 -0800

    Add files via upload

---
+++ Format-Unity/code/docs1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+
+    void Awake()
+    {
+        Debug.Log("ali");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ALI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("ALI :)");
+    }
+}
