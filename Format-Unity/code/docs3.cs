c9967bd3b9924c26f9c202a68e6400d20b955faa
adko <154256737+adkooo@users.noreply.github.com>
2024-03-09 01:03:11 -0800

    docs3.cs

---
+++ Format-Unity/code/docs3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+   // public GameObject a;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //a = GameObject.Find("New Sprite");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      //  a.transform.Rotate(0, 0, 0);
+
+
+        transform.Rotate(1, 0, 0);
+    }
+}
