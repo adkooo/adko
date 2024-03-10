0ef96a34180496389d2bd92b141bac33ae5577d4
adko <154256737+adkooo@users.noreply.github.com>
2024-03-10 04:13:08 -0700

    docs9.cs

---
+++ Format-Unity/code/docs9.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tast4 : MonoBehaviour
+{
+
+
+    int[] a = {5, 7, 9, 2, 4};
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print (a[3]);
+        a[0] = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
