ea6a7f8f676175dc84630a55b2a8389beab9633a
adko <154256737+adkooo@users.noreply.github.com>
2024-03-11 04:42:40 -0700

    docs14.cs

---
+++ Format-Unity/code/docs14.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test4 : MonoBehaviour
+{
+
+
+    //public GameObject a;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Destroy(gameObject);
+        }
+
+        /*
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            Destroy(a);
+        }
+        */
+
+    }
+}
