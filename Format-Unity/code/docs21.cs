878451e6fe3adc2c4ba304b05cd259b9ec8afa87
adko <154256737+adkooo@users.noreply.github.com>
2024-03-12 03:44:07 -0700

    docs21.cs

---
+++ Format-Unity/code/docs21.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test4 : MonoBehaviour
+{
+
+
+    public float a;
+    public Text b;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float c = Input.GetAxis("Vertical");
+        float e = c * a;
+        transform.position = new Vector3( 0, e, 0);
+        b.text = "vale : " + c.ToString("F2");
+    }
+}
