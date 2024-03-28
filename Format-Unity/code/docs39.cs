fefae53b9e695a69808a2e906403297efa157b73
adko <154256737+adkooo@users.noreply.github.com>
2024-03-28 03:13:14 -0700

    docs39.cs

--- Format-Unity/code/docs39.cs
+++ Format-Unity/code/docs39.cs
@@ -10,6 +10,30 @@ public class test4 : MonoBehaviour
 
     public GameObject[] a;
 
+
+    void OnMuseUp()
+    {
+        print(1);
+    }
+
+    void OnMuseEnter()
+    {
+        print(2);
+    }
+
+    void OnMuseExit()
+    {
+        print(3);
+    }
+
+    void OnMuseOver()
+    {
+        print(4);
+        transform.Rotate(0,1,0);
+    }
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
